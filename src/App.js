3cfebcff2dee699688292337451782591590ab09
John Merritt <john_merritt@JohnMerrittiMac.local>
2020-04-29 13:22:49 -0700

    added table ui

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import './components/Grid.js';
 import CenteredGrid from './components/Grid.js';
+import './components/Table.js';
+import ReactVirtualizedTable from './components/Table.js';
 
 function App() {
   return (
     <div className="App">
       {CenteredGrid()}
+      {ReactVirtualizedTable()}
     </div>
   );
 }
