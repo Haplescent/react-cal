78a8a6a0feedf3a7fa344db510f690918d8a94a4
John Merritt <john_merritt@JohnMerrittiMac.targetdiscovery.lan>
2020-05-01 15:12:02 -0700

    moved Button and textfield to same functional component so that state can be shared between them

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,11 +3,15 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import BasicTextFields from "./Textfield.js";
-import ContainedButtons from "./Button.js";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    "& > *": {
+      margin: theme.spacing(1),
+    }
   },
   paper: {
     padding: theme.spacing(0),
@@ -19,6 +23,40 @@ const useStyles = makeStyles((theme) => ({
 export default function CenteredGrid(props) {
   const classes = useStyles();
   const [textInput, setTextInput] = useState("");
+  const [text, setText] = useState("Save");
+
+
+  function handleClick() {
+    if (text === "Save") {
+      setText("Edit")
+    } else {
+      setText("Save")
+    }
+  }
+
+  function ContainedButtons() {
+    return (
+      <Grid item xs={3}>
+      <Paper className={classes.paper}>
+        <div className={classes.root}>
+          <Button variant="contained" onClick={() => handleClick()}>{text}</Button>
+        </div>
+      </Paper>
+      </Grid>
+    );
+  }
+
+  function BasicTextFields() {
+    return (
+      <Grid item xs={6}>
+        <Paper Paper className={classes.paper}>
+          <form className={classes.root} noValidate autoComplete="off">
+            <TextField id="standard-basic" label="Standard" />
+          </form>
+        </Paper>
+      </Grid>
+    );
+  }
 
   return (
     <div className={classes.root}>
@@ -34,12 +72,8 @@ export default function CenteredGrid(props) {
               <h1>{props.hour}</h1>
             </Paper>
           </Grid>
-          <Grid item xs={6}>
-            <Paper Paper className={classes.paper}>{BasicTextFields()}</Paper>
-          </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>{ContainedButtons()}</Paper>
-          </Grid>
+          {BasicTextFields()}
+          {ContainedButtons()}
         </Grid>
     </div>
   );
