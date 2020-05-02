c806c6761357265b33c0ed0609132e001aaa9a41
John Merritt <johnmerritt95@gmail.com>
2020-05-02 09:30:18 -0700

    Updates

---
+++ precache-manifest.9eb24f14e75f3c1bc031940e2b25442d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345bfdad5d4ebd2463ea9aafe1d93a84",
+    "url": "/react-cal/index.html"
+  },
+  {
+    "revision": "04c32f6ea96c39e081d1",
+    "url": "/react-cal/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "15920d73df660ce116e1",
+    "url": "/react-cal/static/js/2.aea652be.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-cal/static/js/2.aea652be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04c32f6ea96c39e081d1",
+    "url": "/react-cal/static/js/main.413e9ac9.chunk.js"
+  },
+  {
+    "revision": "f6a22b42ec422fa2d0af",
+    "url": "/react-cal/static/js/runtime-main.546dfa69.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-cal/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-cal/precache-manifest.9eb24f14e75f3c1bc031940e2b25442d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-cal/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
